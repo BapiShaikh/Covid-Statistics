ffdd587720d8b464db32de79de985d425c5915a9
Bapi Shaikh <bapishaikh1148@gmail.com>
2023-05-28 16:53:38 +0530

    added file

---
+++ src/index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const { collection } = require('./connector');
+const app = express()
+const port = 8080
+// Parse JSON bodies (as sent by API clients)
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+const { connection } = require('./connector')
+
+app.get('/totalRecovered', async (req, res) => {
+    const result = await connection.aggregate([
+      { $group: { _id: 'total', recovered: { $sum: '$recovered' } } }
+    ])
+  
+    res.json({ data: result });
+  });
+
+  app.get('/totalActive', async (req, res) => {
+   const result = await connection.aggregate([
+      {
+        $group: {
+          _id: 'total',
+          active: { $sum: { $subtract: ['$infected', '$recovered'] } }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          active: 1
+        }
+      }
+    ])
+
+    res.json({
+        data:result
+    })
+  });
+
+  
+app.get('/totalDeath', async (req, res) => {
+    const result = await connection.aggregate([
+      { $group: { _id: 'total', death: { $sum: '$death' } } }
+    ]);
+  
+    res.json({ data: result });
+  });
+
+
+  app.get('/hotspotStates', async (req, res) => {
+
+      const result = await connection.aggregate([
+        {
+            $project: {
+              _id: 0,
+              state: 1,
+              rate: { $round: [{ $divide: [{ $subtract: ['$infected', '$recovered'] }, '$infected'] }, 5] },
+            },
+          },
+          { $match: { rate: { $gt: 0.1 } } },
+      ]);
+      res.json({ data: result });
+    
+  });
+  
+  app.get('/healthyStates', async (req, res) => {
+  
+      const result = await connection.aggregate([
+        {
+            $project: {
+              _id: 0,
+              state: 1,
+              mortality: { $round: [{ $divide: ['$death', '$infected'] }, 5] },
+            },
+          },
+          { $match: { mortality: { $lt: 0.005 } } },
+      ])
+      res.json({ data: result });
+   
+  });
+
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+module.exports = app;
